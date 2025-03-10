eb6da19e17427d00838596f46dac1e0bbde23733
luuk <l.j.c.kool@student.tue.nl>
2025-03-10 11:49:25 +0100

    add

--- SensorCode.cc
+++ SensorCode.cc
@@ -6,16 +6,41 @@ std::string topic_pub = "/pos";   //publish to this topic
 gz::transport::Node node;
 auto pub = node.Advertise<gz::msgs::Twist>(topic_pub);
 
+double velx = 0;
+double vely = 0;
+double velz = 0;
+
+double posx = 0;
+double posy = 0;
+double posz = 0;
+
+double dt = 0.01;
+
 void cb(const gz::msgs::IMU &_msg)
 {
 
   gz::msgs::Twist data;
-  double posx = _msg.orientation().x();
-  double posy = _msg.orientation().y();
-  double posz = _msg.orientation().z();
+  double orix = _msg.orientation().x();
+  double oriy = _msg.orientation().y();
+  double oriz = _msg.orientation().z();
+
+  double linax = _msg.linear_acceleration().x();
+  double linay = _msg.linear_acceleration().y();
+  double linaz = _msg.linear_acceleration().z();
+
+  velx=velx+linax * dt;
+
+  posx = posx + velx * dt;
 
 
-  printf("x=%f, y=%f, z=%f\n", posx, posy, posz);
+
+  printf("Orientation:\nx=%f, y=%f, z=%f\n", orix, oriy, oriz);
+  printf("Lin Acc:\nx=%f, y=%f, z=%f\n", linax, linay, linaz);
+  printf("Lin Vel:\nx=%f, y=%f, z=%f\n", velx, vely, velz);
+  printf("Lin pos:\nx=%f, y=%f, z=%f\n\n\n", posx, posy, posz);
+  printf("------------------------------------\n");
+
+
 
   pub.Publish(data);
 }
